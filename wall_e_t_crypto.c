1d075d9e7d4472c121e5ff681c44e52f0066d1e7
Rubberazer <47650457+Rubberazer@users.noreply.github.com>
2025-01-08 19:04:26 +0100

    Error management

--- wall_e_t_crypto.c
+++ wall_e_t_crypto.c
@@ -18,10 +18,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include <wall_e_t.h>
 
 gcry_error_t libgcrypt_initializer(void) {
-	static gcry_error_t err = 0;
+	static gcry_error_t err = GPG_ERR_NO_ERROR;
 
     // Check libgcrypt version	
 	const char *version = gcry_check_version(NEED_LIBGCRYPT_VERSION);
@@ -65,12 +66,12 @@ gcry_error_t libgcrypt_initializer(void) {
 }
 
 gcry_error_t char_to_uint8(char *s_string, uint8_t *s_number, size_t string_length) {
-	static gcry_error_t err = 0;
+	static gcry_error_t err = GPG_ERR_NO_ERROR;
 	char *string_swap = NULL;
 
 	string_swap = (char *)gcry_calloc_secure(5, sizeof(char));
 	if (string_swap == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr1;
 	}
 
@@ -86,12 +87,12 @@ allocerr1:
 }
 
 gcry_error_t uint8_to_char(uint8_t *s_number, char *s_string, size_t uint8_length) {
-	static gcry_error_t err = 0;
+	static gcry_error_t err = GPG_ERR_NO_ERROR;
 	char *string_swap = NULL;
 
 	string_swap = (char *)gcry_calloc_secure(5, sizeof(char));
 	if (string_swap == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr1;
 	}
 
@@ -107,12 +108,12 @@ allocerr1:
 }
 
 gcry_error_t hash_to_hash160(uint8_t *hash160, uint8_t *hex, size_t hex_length) {
-	static gcry_error_t err = 0;
+	static gcry_error_t err = GPG_ERR_NO_ERROR;
 	uint8_t *s_buff = NULL;
 	
 	s_buff = (uint8_t *)gcry_calloc_secure(gcry_md_get_algo_dlen(GCRY_MD_SHA256), sizeof(uint8_t));
 	if (s_buff == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr1;
 	}	
 	
@@ -126,7 +127,7 @@ allocerr1:
 
 gcry_error_t base58_encode(char *base58, uint8_t *key, size_t uint8_length, size_t char_length) {
 	#define STRING_SWAP 200
-	static gcry_error_t err = 0;
+	static gcry_error_t err = GPG_ERR_NO_ERROR;
 	gcry_mpi_t mpi_base58 = NULL;
 	gcry_mpi_t mpi_key = NULL;
 	gcry_mpi_t mpi_result = NULL;
@@ -137,32 +138,32 @@ gcry_error_t base58_encode(char *base58, uint8_t *key, size_t uint8_length, size
 	
 	mpi_base58 = gcry_mpi_snew(8);
 	if (mpi_base58 == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr1;
 	}
 	mpi_key = gcry_mpi_snew(uint8_length*8);
 	if (mpi_key == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr2;
 	}
 	mpi_result = gcry_mpi_snew(uint8_length*8);
 	if (mpi_result == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr3;
 	}
 	mpi_mod = gcry_mpi_snew(8);
 	if (mpi_mod == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr4;
 	}
 	string_swap = (char *)gcry_calloc_secure(STRING_SWAP, sizeof(char));
 	if (string_swap == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr5;
 	}
 	uint8_swap = (uint32_t *)gcry_calloc_secure(1, sizeof(uint32_t));
 	if (uint8_swap == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr6;
 	}
 
@@ -170,12 +171,13 @@ gcry_error_t base58_encode(char *base58, uint8_t *key, size_t uint8_length, size
 	err = gcry_mpi_scan(&mpi_key, GCRYMPI_FMT_USG, key, uint8_length, NULL);
 	if (err) {
 		fprintf(stderr, "Failed to scan key to mpi format\n");
+		goto allocerr7;
 	}
 	ssize_t comp = gcry_mpi_cmp_ui(mpi_key, 0);
 	if (!comp) {
 		fprintf(stderr, "Key is zero\n");
-		err = -2;
-		return err;
+		err = gcry_error_from_errno(EINVAL);
+		goto allocerr7;
 	}
 	
 	size_t counter = 0;
@@ -192,6 +194,7 @@ gcry_error_t base58_encode(char *base58, uint8_t *key, size_t uint8_length, size
 		err = gcry_mpi_get_ui(uint8_swap, mpi_mod);
 		if (err) {
 			fprintf(stderr, "Failed operation key mod 58\n");
+			goto allocerr7;
 		}
 		memcpy(string_swap+STRING_SWAP-i, &base58_arr[*uint8_swap], 1);		
 		mpi_key = gcry_mpi_set(mpi_key, mpi_result);
@@ -201,7 +204,8 @@ gcry_error_t base58_encode(char *base58, uint8_t *key, size_t uint8_length, size
 		}
 	}
 	strncpy(base58+strlen(base58), string_swap+counter, char_length-strlen(base58));
-		
+
+allocerr7:
 	gcry_free(uint8_swap);
 allocerr6:
 	gcry_free(string_swap);
@@ -220,7 +224,7 @@ allocerr1:
 
 gcry_error_t pub_from_priv(uint8_t *pub_key, uint8_t *pub_key_c, uint8_t *priv_key) {
 	#define BUFF_SIZE 400
-	static gcry_error_t err = 0;
+	static gcry_error_t err = GPG_ERR_NO_ERROR;
 	char *s_key_buff = NULL;
 	char *s_key_swap = NULL;
 	gcry_ctx_t s_key_ctx = NULL;
@@ -229,27 +233,27 @@ gcry_error_t pub_from_priv(uint8_t *pub_key, uint8_t *pub_key_c, uint8_t *priv_k
 
 	s_key_buff = (char *)gcry_calloc_secure(BUFF_SIZE, sizeof(char));
 	if (s_key_buff == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr1;
 	}	
 	s_key_swap = (char *)gcry_calloc_secure(BUFF_SIZE, sizeof(char));
 	if (s_key_swap == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr2;
 	}	
 	s_key_ctx = (gcry_ctx_t)gcry_calloc_secure(1, sizeof(gcry_ctx_t));
 	if (s_key_ctx == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr3;
 	}
 	s_key_pub = (gcry_sexp_t)gcry_calloc_secure(1, sizeof(gcry_sexp_t));
 	if (s_key_pub == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr4;
 	}
 	s_key = (gcry_sexp_t)gcry_calloc_secure(1, sizeof(gcry_sexp_t));
 	if (s_key == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr5;
 	}
 
@@ -265,14 +269,17 @@ gcry_error_t pub_from_priv(uint8_t *pub_key, uint8_t *pub_key_c, uint8_t *priv_k
 	err = gcry_sexp_new(&s_key, s_key_buff, 0, 1);
 	if (err) {
 		fprintf(stderr, "Failed to create s-expression for master key\n");
+		goto allocerr6;
 	}
 	err = gcry_mpi_ec_new(&s_key_ctx, s_key, NULL);
 	if (err) {
 		fprintf(stderr, "Failed to create context type for master key\n");
+		goto allocerr6;
 	}
 	err = gcry_pubkey_get_sexp(&s_key_pub, GCRY_PK_GET_PUBKEY, s_key_ctx);
 	if (err) {
 		fprintf(stderr, "Failed to extract public key from context\n");
+		goto allocerr6;
 	}
 
 	memset(s_key_buff, 0, BUFF_SIZE);
@@ -285,13 +292,15 @@ gcry_error_t pub_from_priv(uint8_t *pub_key, uint8_t *pub_key_c, uint8_t *priv_k
 	err = char_to_uint8(s_key_buff, pub_key, strlen(s_key_buff));
 	if (err) {
 		fprintf(stderr, "Failed to convert public key into a numerical format\n");
+		goto allocerr6;
 	}	
 	s_key_buff = P;
 
 	memcpy(pub_key_c, pub_key, PUBKEY_LENGTH);
 	// Parity
 	pub_key_c[0] = (pub_key[64]%2) ? 0x03 : 0x02;
-		
+
+allocerr6:
 	gcry_sexp_release(s_key);
 allocerr5:
 	gcry_sexp_release(s_key_pub);
@@ -309,7 +318,7 @@ allocerr1:
 
 gcry_error_t create_mnemonic(char *salt, uint8_t nwords, mnemonic_t *mnem) {
 	typedef char *word_t[nwords];
-	static gcry_error_t err = 0;
+	static gcry_error_t err = GPG_ERR_NO_ERROR;
 	char *wordlist[] = {WORDLIST};
 	uint32_t nbytes = 0;
 	uint8_t *r_seed = NULL;
@@ -322,12 +331,12 @@ gcry_error_t create_mnemonic(char *salt, uint8_t nwords, mnemonic_t *mnem) {
 
 	if (salt == NULL || strlen(salt) > 20) {
 		fprintf (stderr, "Salt is limited to 20 characters in english, no emoticons\n");
-		err = -1;
+		err = gcry_error_from_errno(EINVAL);
 		return err;
 	}
 	if (mnem == NULL) {
 		fprintf(stderr, "Mnem can´t be NULL\n");
-		err = -2;
+		err = gcry_error_from_errno(EINVAL);
 		return err;
 	}	
 	
@@ -344,38 +353,38 @@ gcry_error_t create_mnemonic(char *salt, uint8_t nwords, mnemonic_t *mnem) {
 		break;
 	default:
 		fprintf (stderr, "Number of words should be either: 12, 15, 18, 21 or 24\n");
-		err = -1;
+		err = gcry_error_from_errno(EINVAL);
 		return err;
 	}
 
 	r_seed = (uint8_t *)gcry_calloc_secure(nbytes, 1);
 	if (r_seed == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr1;
 	}
 	h_seed = (uint8_t *)gcry_calloc_secure(gcry_md_get_algo_dlen(GCRY_MD_SHA256), 1);
 	if (h_seed == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr2;
 	}
 	e_seed = (uint8_t *)gcry_calloc_secure((nbytes+1), 1);
 	if (e_seed == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr3;
 	}
 	s_swap = (uint32_t *)gcry_calloc_secure(1, sizeof(uint32_t));
 	if (s_swap == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr4;
 	}
 	words = (word_t *)gcry_calloc_secure(1, sizeof(word_t)+50*nwords);
 	if (words == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr5;
 	}
 	key_buff = (gcry_buffer_t *)gcry_calloc_secure(2, sizeof(gcry_buffer_t));
 	if (key_buff == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr6;
 	}	
 		
@@ -406,6 +415,7 @@ gcry_error_t create_mnemonic(char *salt, uint8_t nwords, mnemonic_t *mnem) {
 	err = gcry_kdf_derive(mnem->mnemonic, strlen(mnem->mnemonic), GCRY_KDF_PBKDF2, GCRY_MD_SHA512, s_salt, strlen(s_salt), PBKDF2_ITERN, gcry_md_get_algo_dlen(GCRY_MD_SHA512), mnem->seed);
 	if (err) {
 		fprintf(stderr, "Failed to derive seed\n");
+		goto allocerr7;
 	}
 
 	key_buff[0].len = strlen("Bitcoin seed");
@@ -416,6 +426,7 @@ gcry_error_t create_mnemonic(char *salt, uint8_t nwords, mnemonic_t *mnem) {
 	err = gcry_md_hash_buffers(GCRY_MD_SHA512, GCRY_MD_FLAG_HMAC, mnem->keys.key_priv_chain, key_buff, 2);
 	if (err) {
 		fprintf(stderr, "Failed to produce master key with chain code\n");
+		goto allocerr7;
 	}
 
 	memcpy(mnem->keys.key_priv, mnem->keys.key_priv_chain, PRIVKEY_LENGTH);
@@ -424,10 +435,12 @@ gcry_error_t create_mnemonic(char *salt, uint8_t nwords, mnemonic_t *mnem) {
 	err = pub_from_priv(mnem->keys.key_pub, mnem->keys.key_pub_comp, mnem->keys.key_priv);
 	if (err) {
 		fprintf(stderr, "Failed to produce public key from private\n");
+		goto allocerr7;
 	}
 
 	mnem->keys.key_index = 0x00; 
-	
+
+allocerr7:
 	gcry_free(key_buff);	
 allocerr6:
 	gcry_free(words);
@@ -445,7 +458,7 @@ allocerr1:
 }
 
 gcry_error_t key_deriv(key_pair_t *child_keys, uint8_t *parent_priv_key, uint8_t *parent_chain_code, size_t key_index, hardened_t hardened) {
-	static gcry_error_t err = 0;
+	static gcry_error_t err = GPG_ERR_NO_ERROR;
 	gcry_buffer_t *key_buff = NULL;
 	uint8_t *swap_priv_key = NULL;
 	uint32_t *index = NULL;
@@ -459,68 +472,68 @@ gcry_error_t key_deriv(key_pair_t *child_keys, uint8_t *parent_priv_key, uint8_t
 	
 	if (key_index < 0) {
 		fprintf(stderr, "Derived key index should be bigger than 0\n");
-		err = -2;
+		err = gcry_error_from_errno(EINVAL);
 		return err;
 	}
 	if (hardened < 0 || hardened > 1) {
 		fprintf(stderr, "Hardened should be either \"hardened\" or \"normal\"\n");
-		err = -2;
+		err = gcry_error_from_errno(EINVAL);
 		return err;
 	}
 	if (child_keys == NULL || parent_priv_key == NULL || parent_chain_code == NULL) {
 		fprintf(stderr, "Child_keys, parent_priv_key and parent_chain_code can't be NULL\n");
-		err = -2;
+		err = gcry_error_from_errno(EINVAL);
 		return err;
 	}	
 	
 	key_buff = (gcry_buffer_t *)gcry_calloc_secure(2, sizeof(gcry_buffer_t));
 	if (key_buff == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr1;
 	}	
 	swap_priv_key = (uint8_t *)gcry_calloc_secure(37, sizeof(uint8_t));
 	if (swap_priv_key == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr2;
 	}		
 	index = (uint32_t *)gcry_calloc_secure(1, sizeof(uint32_t));
 	if (index == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr3;
 	}
 	intermediate_key = (uint8_t *)gcry_calloc_secure(64, sizeof(uint8_t));
 	if (intermediate_key == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr4;
 	}
 	interm_key = gcry_mpi_snew(256);
 	if (interm_key == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr5;
 	}
 	order_sec = gcry_mpi_snew(256);
 	if (order_sec == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr6;
 	}
 	key_par = gcry_mpi_snew(256);
 	if (key_par == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr7;
 	}
 	key_child = gcry_mpi_snew(256);
 	if (key_child == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr8;
 	}
 	key_mod_result = gcry_mpi_snew(256);
 	if (key_mod_result == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr9;
 	}
 	n_secp256k1 = (uint8_t *)gcry_calloc_secure(32, sizeof(uint8_t));
 	if (n_secp256k1 == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr10;
 	}
 	
@@ -544,53 +557,64 @@ gcry_error_t key_deriv(key_pair_t *child_keys, uint8_t *parent_priv_key, uint8_t
 	err = gcry_md_hash_buffers(GCRY_MD_SHA512, GCRY_MD_FLAG_HMAC, intermediate_key, key_buff, 2);
 	if (err) {
 		fprintf(stderr, "Failed to HMAC child private key\n");
+		goto allocerr11;
 	}
 
 	memcpy(child_keys->chain_code, intermediate_key+PRIVKEY_LENGTH, CHAINCODE_LENGTH);
 	err = gcry_mpi_scan(&interm_key, GCRYMPI_FMT_USG, intermediate_key, CHAINCODE_LENGTH, NULL);
 	if (err) {
 		fprintf(stderr, "Failed to scan intermediate key to mpi format\n");
+		goto allocerr11;
 	}
 	err = char_to_uint8(N_SECP256K1, n_secp256k1, 64);	
 	if (err) {
 		fprintf(stderr, "Failed to convert N_SECP256K1 to unsigned char\n");
+		goto allocerr11;
 	}	
 	err = gcry_mpi_scan(&order_sec, GCRYMPI_FMT_USG, n_secp256k1, 32, NULL);
 	if (err) {
 		fprintf(stderr, "Failed to scan N_SECP256K1 to mpi format\n");
+		goto allocerr11;
 	}
 	
 	ssize_t comp = gcry_mpi_cmp(interm_key, order_sec);	
 	if (comp > 0) {
 		fprintf(stderr, "Child key is invalid, use the next index value\n");
-		err = -2;
+		err = gcry_error_from_errno(EINVAL);
+		goto allocerr11;
 	}
 	comp = gcry_mpi_cmp_ui(interm_key, 0x0);
 	if (!comp) {
 		fprintf(stderr, "Child key is invalid, use the next index value\n");
-		err = -2;
+		err = gcry_error_from_errno(EINVAL);
+		goto allocerr11;
 	}
 
 	err = gcry_mpi_scan(&key_par, GCRYMPI_FMT_USG, parent_priv_key, PRIVKEY_LENGTH, NULL);
 	if (err) {
 		fprintf(stderr, "Failed to scan N_SECP256K1 to mpi format\n");
+		goto allocerr11;
 	}
 	err = gcry_mpi_scan(&key_child, GCRYMPI_FMT_USG, intermediate_key, PRIVKEY_LENGTH, NULL);
 	if (err) {
 		fprintf(stderr, "Failed to scan N_SECP256K1 to mpi format\n");
+		goto allocerr11;
 	}	
 	
 	gcry_mpi_addm(key_mod_result, key_child, key_par, order_sec);
 	err = gcry_mpi_print(GCRYMPI_FMT_USG, child_keys->key_priv, PRIVKEY_LENGTH, NULL, key_mod_result); 
 	if (err) {
 		fprintf(stderr, "Failed to modulo the child private key\n");
+		goto allocerr11;
 	}
 		
 	err = pub_from_priv(child_keys->key_pub, child_keys->key_pub_comp, child_keys->key_priv);
 	if (err) {
 		fprintf(stderr, "Failed to derive child public from private child key\n");
+		goto allocerr11;
 	}
 
+allocerr11:
 	gcry_free(n_secp256k1);
 allocerr10:
 	gcry_mpi_release(key_mod_result);
@@ -616,7 +640,7 @@ allocerr1:
 }
 
 gcry_error_t ext_keys_address(key_address_t *keys_address, key_pair_t *keys, uint8_t *par_pub, uint8_t depth, BIP_t wallet_type)  {
-	static gcry_error_t err = 0;
+	static gcry_error_t err = GPG_ERR_NO_ERROR;
 	uint8_t *intermediate_key = NULL;
 	uint8_t *hash160 = NULL;
 	uint8_t *checksum = NULL;
@@ -625,28 +649,28 @@ gcry_error_t ext_keys_address(key_address_t *keys_address, key_pair_t *keys, uin
 	
 	if (depth < 0) {
 		fprintf(stderr, "Depth should be 0 or higher\n");
-		err = -2;
+		err = gcry_error_from_errno(EINVAL);
 		return err;
 	}
 	if (keys_address == NULL || keys == NULL || keys_address == NULL) {
 		fprintf(stderr, "Keys_address and keys can't be NULL\n");
-		err = -2;
+		err = gcry_error_from_errno(EINVAL);
 		return err;
 	}
 	
 	intermediate_key = (uint8_t *)gcry_calloc_secure(INTER_KEY+CHECKSUM, sizeof(uint8_t));
 	if (intermediate_key == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr1;
 	}
 	hash160 = (uint8_t *)gcry_calloc_secure(HASH160_LENGTH, sizeof(uint8_t));
 	if (hash160 == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr2;
 	}
 	checksum = (uint8_t *)gcry_calloc_secure(PRIVKEY_LENGTH, sizeof(uint8_t));
 	if (checksum == NULL) {
-		err = -1;
+		err = gcry_error_from_errno(ENOMEM);
 		goto allocerr3;
 	}	
 	
@@ -654,6 +678,7 @@ gcry_error_t ext_keys_address(key_address_t *keys_address, key_pair_t *keys, uin
 		err = hash_to_hash160(hash160, par_pub, PUBKEY_LENGTH);
 		if (err) {
 			fprintf(stderr, "Failed to create fingerprint from parent key\n");
+			goto allocerr4;
 		}	
 	}
 	else {
@@ -687,6 +712,7 @@ gcry_error_t ext_keys_address(key_address_t *keys_address, key_pair_t *keys, uin
 	err = char_to_uint8(BIP_PRV, intermediate_key, 8);
 	if (err) {
 		fprintf(stderr, "Failed to convert intermediate key to numerical format\n");
+		goto allocerr4;
 	}	
 	memcpy(intermediate_key+13, keys->chain_code, CHAINCODE_LENGTH);
 	memcpy(intermediate_key+46, keys->key_priv, PRIVKEY_LENGTH);
@@ -698,16 +724,12 @@ gcry_error_t ext_keys_address(key_address_t *keys_address, key_pair_t *keys, uin
 	}
 
 	memcpy(intermediate_key+INTER_KEY, checksum, CHECKSUM);
-
-	/*printf("\nPrinting intermediate key priv: \n");
-	for (uint32_t i = 0; i < 82; i++) {
-		printf("%02x", intermediate_key[i]);
-		}*/
 	
 	// Here private address
 	err = base58_encode(keys_address->xpriv, intermediate_key, INTER_KEY+CHECKSUM, sizeof(keys_address->xpriv)/sizeof(char));
 	if (err) {
 		fprintf(stderr, "Failed to convert intermediate priv key to address format\n");
+		goto allocerr4;
 	}
 
 	//Public key
@@ -715,6 +737,7 @@ gcry_error_t ext_keys_address(key_address_t *keys_address, key_pair_t *keys, uin
 	err = char_to_uint8(BIP_PUB, intermediate_key, 8);
 	if (err) {
 		fprintf(stderr, "Failed to convert intermediate key to numerical format\n");
+		goto allocerr4;
 	}	
 	memcpy(intermediate_key+13, keys->chain_code, CHAINCODE_LENGTH);
 	memcpy(intermediate_key+45, keys->key_pub_comp, PUBKEY_LENGTH);
@@ -722,17 +745,14 @@ gcry_error_t ext_keys_address(key_address_t *keys_address, key_pair_t *keys, uin
 	gcry_md_hash_buffer(GCRY_MD_SHA256, checksum, checksum, gcry_md_get_algo_dlen(GCRY_MD_SHA256));
 	memcpy(intermediate_key+INTER_KEY, checksum, CHECKSUM);
 	
-	/*printf("\nPrinting intermediate key pub : \n");
-	for (uint32_t i = 0; i < 82; i++) {
-		printf("%02x", intermediate_key[i]);
-		}*/
-
 	// Here public address	
 	err = base58_encode(keys_address->xpub, intermediate_key, INTER_KEY+CHECKSUM, sizeof(keys_address->xpub)/sizeof(char));
 	if (err) {
 		fprintf(stderr, "Failed to convert intermediate pub key to address format\n");
+		goto allocerr4;
 	}
-	
+
+allocerr4:
 	gcry_free(checksum);
 allocerr3:
 	gcry_free(hash160);	
