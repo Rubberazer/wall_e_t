c44d25812bab28ae459b0df572272cdb86bf92d7
Rubberazer <47650457+Rubberazer@users.noreply.github.com>
2025-01-26 18:53:14 +0100

    deriving BIP84 addresses

--- src/test_BIP84.c
+++ src/test_BIP84.c
@@ -18,41 +18,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include <wall_e_t.h>
 
 int main(void) {
     static gcry_error_t err = 0;    
 
-    // m'/84'/0'/0'/0
+    /********************
+    *  m'/84'/0'/0'/0   *
+    *********************/
 
     mnemonic_t *mnem = NULL;
-    key_pair_t *purpose_keys = NULL;
-    key_pair_t *coin_keys = NULL;
-    key_pair_t *account_keys = NULL;
-    key_pair_t *receive_keys = NULL;
-    key_pair_t *change_keys = NULL;
-    key_address_t *root_address = NULL;
-    key_address_t *account_address = NULL; 
+    key_pair_t *child_keys = NULL;
+    //key_address_t *root_address = NULL;
+    //key_address_t *account_address = NULL; 
     
     if (!libgcrypt_initializer()) {
 	exit(EXIT_FAILURE);
     }
 
     mnem = (mnemonic_t *)gcry_calloc_secure(1, sizeof(mnemonic_t));
-    if (mnem == NULL)
-	exit(EXIT_FAILURE);
+    if (mnem == NULL) {
+	err = gcry_error_from_errno(ENOMEM);
+	goto allocerr1;
+    }
+    child_keys = (key_pair_t *)gcry_calloc_secure(5, sizeof(key_pair_t));
+    if (child_keys == NULL) {
+	err = gcry_error_from_errno(ENOMEM);
+	goto allocerr2;
+    }
+
+    // Obtain mnemonic + root keys
+    err = create_mnemonic("xxxx", 12, mnem);
+    if (err) {
+	printf("Problem creating mnemonic, error code:%s, %s", gcry_strerror(err), gcry_strsource(err));
+    }
+    // Deriving keys
+    // Purpose BIP84
+    err = key_deriv(&child_keys[0], mnem->keys.key_priv, mnem->keys.chain_code, BIP84, hardened_child);
+    if (err) {
+	printf("Problem deriving child key, error code:%s, %s", gcry_strerror(err), gcry_strsource(err));
+    }	
+    // Coin Bitcoin
+    err = key_deriv(&child_keys[1], (uint8_t *)(&child_keys[0].key_priv), (uint8_t *)(&child_keys[0].chain_code), COIN_BITCOIN, hardened_child);
+    if (err) {
+	printf("Problem deriving child key, error code:%s, %s", gcry_strerror(err), gcry_strsource(err));
+    }	
+    // Account keys
+    err = key_deriv(&child_keys[2], (uint8_t *)(&child_keys[1].key_priv), (uint8_t *)(&child_keys[1].chain_code), ACCOUNT, hardened_child);
+    if (err) {
+	printf("Problem deriving child key, error code:%s, %s", gcry_strerror(err), gcry_strsource(err));
+    }	
+    // Receive keys
+    err = key_deriv(&child_keys[3], (uint8_t *)(&child_keys[2].key_priv), (uint8_t *)(&child_keys[2].chain_code), 0, normal_child);
+    if (err) {
+	printf("Problem deriving child key, error code:%s, %s", gcry_strerror(err), gcry_strsource(err));
+    }	
+    // Change keys
+    err = key_deriv(&child_keys[4], (uint8_t *)(&child_keys[2].key_priv), (uint8_t *)(&child_keys[2].chain_code), 1, normal_child);
+    if (err) {
+	printf("Problem deriving child key, error code:%s, %s", gcry_strerror(err), gcry_strsource(err));
+    }	
+
     
     
-    
-    gcry_free(change_keys);
-    gcry_free(receive_keys);
-    gcry_free(account_keys);
-    gcry_free(coin_keys);
-    gcry_free(purpose_keys);
+    gcry_free(child_keys);
+ allocerr2:
     gcry_free(mnem);
-	
+ allocerr1:
     gcry_control(GCRYCTL_TERM_SECMEM);
 
-    printf("\nExiting out\n");
     exit(EXIT_SUCCESS);
 }
